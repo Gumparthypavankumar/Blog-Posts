86af743c26f389cf44cfafaf0124c88146ab6af7
Gumparthy Pavan Kumar <gumparthypk@gmail.com>
2020-09-14 08:36:13 +0530

    Updated

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports={
-        mongoURI : 'mongodb+srv://PavanKumar:Pavan@123@blogposts-prod.uvl3c.mongodb.net/<dbname>?retryWrites=true&w=majority'
+        mongoURI : 'mongodb+srv://PavanKumar:Pavan123@blogposts-prod.uvl3c.mongodb.net/<dbname>?retryWrites=true&w=majority'
     }
 } else {
     module.exports ={mongoURI : 'mongodb://localhost/blogs'}
