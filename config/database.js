a1409c0bcc2489ab106b7c2fe955b59e1eb606c3
PavanKumar <51289314+Gumparthypavankumar@users.noreply.github.com>
2022-02-20 10:24:10 +0530

    fix

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://PavanKumar:oqZ36k2428uDpitr@blogposts-prod.650gh.mongodb.net/blogposts?retryWrites=true&w=majority",
+      "mongodb+srv://<username>:<password>@blogposts-prod.650gh.mongodb.net/blogposts?retryWrites=true&w=majority",
   };
 } else {
   module.exports = {
     mongoURI:
-      "mongodb+srv://PavanKumar:oqZ36k2428uDpitr@blogposts-prod.650gh.mongodb.net/blogposts?retryWrites=true&w=majority",
+      "mongodb+srv://<username>:<password>@blogposts-prod.650gh.mongodb.net/blogposts?retryWrites=true&w=majority",
   };
 }
